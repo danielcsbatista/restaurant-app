316a2c3a13fc7fea684401453eca6dc9a8941ba7
danielcsbatista <danielcsbatista@gmail.com>
2025-11-11 21:06:07 -0300

    feat: Add products component

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -9,53 +9,73 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
+import Products from "./products";
+
 interface RestaurantCategoriesProps {
   restaurant: Prisma.RestaurantGetPayload<{
     include: { MenuCategory: { include: { Product: true } } };
   }>;
 }
 
-const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
-    const [selectedCategory, setSelectedCategory] = useState<MenuCategory | null>(restaurant.MenuCategory[0] || null);
-    
-    const handleCategorySelect = (category: MenuCategory) => {
-      setSelectedCategory(category);
-    }
+type MenuCategoryWithProducts = Prisma.MenuCategoryGetPayload<{
+  include: { Product: true };
+}>;
 
-    const getCategoryButtonVariant = (category: MenuCategory) => selectedCategory?.id === category.id ? "default" : "secondary"
-   
-    return (
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  const [selectedCategory, setSelectedCategory] =
+    useState<MenuCategoryWithProducts | null>(
+      restaurant.MenuCategory[0] || null
+    );
+
+  const handleCategorySelect = (category: MenuCategoryWithProducts) => {
+    setSelectedCategory(category);
+  };
+
+  const getCategoryButtonVariant = (category: MenuCategoryWithProducts) =>
+    selectedCategory?.id === category.id ? "default" : "secondary";
+
+  return (
     <div className="relative z-50 rounded-t-3xl border bg-white mt-[-1.5rem]">
-    <div className="p-5">
+      <div className="p-5">
         <div className="flex items-center gap-3">
-            <Image
+          <Image
             src={restaurant.avatarImageUrl}
             alt={restaurant.name}
             width={45}
             height={45}
-            />
-            <div>
+          />
+          <div>
             <h2 className="text-lg font-semibold">{restaurant.name}</h2>
             <p className="text-xs opacity-55">{restaurant.description}</p>
-            </div>
+          </div>
         </div>
         <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
-            <ClockIcon size="12" />
-            <p>Aberto!</p>
+          <ClockIcon size="12" />
+          <p>Aberto!</p>
         </div>
       </div>
       {selectedCategory && (
-      <ScrollArea className="w-full">
-        <div className="flex w-max space-x-4 p-4 pt-0">
+        <ScrollArea className="w-full">
+          <div className="flex w-max space-x-4 p-4 pt-0">
             {restaurant.MenuCategory.map((category) => (
-              <Button onClick={() => handleCategorySelect(category)} key={category.id} variant={getCategoryButtonVariant(category)} size="sm">
+              <Button
+                onClick={() => handleCategorySelect(category)}
+                key={category.id}
+                variant={getCategoryButtonVariant(category)}
+                size="sm"
+              >
                 {category.name}
               </Button>
             ))}
-        </div>
-        <Scrollbar orientation="horizontal" />
-      </ScrollArea>
+          </div>
+          <Scrollbar orientation="horizontal" />
+        </ScrollArea>
       )}
+      <h3 className="px-5 pt-4 pb-2 text-lg font-semibold">
+        {selectedCategory?.name}
+      </h3>
+
+      <Products products={selectedCategory?.Product || []} />
     </div>
   );
 };
